c51e221148870309b3810632015d5208bc98e6cf
Ramseths <juan.ramseths@gmail.com>
2022-07-29 18:09:14 -0500

    feat: añade img y funcionalidad de scroll, slides

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.about', { delay: 200 });
+sr.reveal('.tech', { delay: 200 });
+sr.reveal('.brand-logo', { interval: 200 })
